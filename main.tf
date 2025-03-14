10df5f5cf2f9b9e1e80e4d7c8bbd4a2cf4d9dceb
Martin Fabre <lucian.clain@gmail.com>
2025-03-14 15:45:31 +0100

    terraform working please now

--- main.tf
+++ main.tf
@@ -1,6 +1,29 @@
 # Azure
 provider "azurerm" {
-  features {}
+  features {
+    /*resource_group {
+      prevent_deletion_if_contains_resources = false
+    }*/
+  }
+  #subscription_id = "for deletion purpose"
+}
+
+# Helm for GitOps
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.rg-test.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.rg-test.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.rg-test.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.rg-test.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+# Kubernetes provider for interacting with the cluster
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.rg-test.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.rg-test.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.rg-test.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.rg-test.kube_config.0.cluster_ca_certificate)
 }
 
 # ResourceGroup creation
@@ -50,26 +73,91 @@ resource "azurerm_role_assignment" "aks_acr_binding" {
   depends_on           = [azurerm_kubernetes_cluster.rg-test]
 }
 
-# Flux installation (only install the base components)
-module "flux_install" {
-  source     = "./flux-install"
-  kubeconfig = azurerm_kubernetes_cluster.rg-test.kube_config_raw
-  fluxcd_key = var.fluxcd_key
-  fluxcd_key_pub = var.fluxcd_key_pub
-  known_hosts = var.known_hosts
+
+# GitOps: FluxCD
+resource "helm_release" "flux" {
+  name       = "flux"
+  repository = "https://fluxcd-community.github.io/helm-charts"
+  chart      = "flux2"
+  namespace  = "flux-system"
+
+  create_namespace = true
+
+  depends_on = [
+    azurerm_kubernetes_cluster.rg-test
+  ]
 }
+
+resource "kubernetes_secret" "flux_git_auth" {
+  metadata {
+    name      = "fluxcd-key"
+    namespace = "flux-system"
+  }
+
+  type = "Opaque"
+  data = {
+    identity       = var.fluxcd_key
+    "identity.pub" = var.fluxcd_key_pub
+    known_hosts    = var.known_hosts
+  }
+
+  depends_on = [helm_release.flux]
+}
+
+# Ressource GitRepository pour FluxCD // A jouer après avoir déployer l'infra + flux
+/*resource "kubernetes_manifest" "flux_git_repository" {
+  manifest = {
+    apiVersion = "source.toolkit.fluxcd.io/v1beta1"
+    kind       = "GitRepository"
+    metadata = {
+      name      = "flux-repo"
+      namespace = "flux-system"
+    }
+    spec = {
+      url       = var.gitops_repo_url
+      secretRef = { name = "fluxcd-key" }
+      interval  = "1m"
+      ref = {
+        branch = "main"
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.flux,
+    kubernetes_secret.flux_git_auth,
+    azurerm_kubernetes_cluster.rg-test
+  ]
+}
+
+resource "kubernetes_manifest" "flux_kustomization" {
+  manifest = {
+    apiVersion = "kustomize.toolkit.fluxcd.io/v1beta1"
+    kind       = "Kustomization"
+    metadata = {
+      name      = "kustomization"
+      namespace = "flux-system"
+    }
+    spec = {
+      interval = "10m"
+      path     = "."
+      sourceRef = {
+        kind = "GitRepository"
+        name = "flux-repo"
+      }
+      targetNamespace = "default"
+      prune = true
+    }
+  }
+
+  depends_on = [
+    kubernetes_manifest.flux_git_repository
+  ]
+}*/
+
 
 # Output: Display kubeconfig infos to connect
 output "kubeconfig" {
   value     = azurerm_kubernetes_cluster.rg-test.kube_config_raw
   sensitive = true
-}
-
-# Output the cluster name and resource group for use in CI/CD
-output "cluster_name" {
-  value = azurerm_kubernetes_cluster.rg-test.name
-}
-
-output "resource_group_name" {
-  value = azurerm_resource_group.rg-test.name
 }
