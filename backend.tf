69a32ca3e22d08d12f6d0ddf8fa47c77f3db2f10
Martin Fabre <lucian.clain@gmail.com>
2025-07-31 15:06:28 +0200

    backend

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-1"
+    storage_account_name = "tfbackend62400"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
