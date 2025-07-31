e4feb524aae9040311c1fad9230c32cc8b853514
Martin Fabre <lucian.clain@gmail.com>
2025-07-31 15:56:08 +0200

    rg-backend-specific

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-1"
+    resource_group_name  = "rg-terraform-backend"
     storage_account_name = "tfbackend62400"
     container_name       = "tfstate"
     key                  = "infra.terraform.tfstate"
