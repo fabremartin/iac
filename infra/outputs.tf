fd5cda1c6e0639144d84411e2ddcc45ebbaed39b
Martin Fabre <lucian.clain@gmail.com>
2025-07-31 16:13:45 +0200

    outputs.tf

---
+++ infra/outputs.tf
@@ -0,0 +1,20 @@
+# infra/outputs.tf
+output "kube_config_host" {
+  value = azurerm_kubernetes_cluster.main.kube_config.0.host
+  sensitive = true
+}
+
+output "kube_config_client_certificate" {
+  value = azurerm_kubernetes_cluster.main.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config_client_key" {
+  value = azurerm_kubernetes_cluster.main.kube_config.0.client_key
+  sensitive = true
+}
+
+output "kube_config_cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate
+  sensitive = true
+}
